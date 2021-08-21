99983e2066ccb0c9c031223fd707356ce48d3357
Luc Sinet <luc.sinet@gmail.com>
2021-08-21 13:47:11 +0200

    Add logger pre-configuration capabilities
    Structlog module now exposes a configure() and a get_logger() method

--- lua/structlog/init.lua
+++ lua/structlog/init.lua
@@ -6,4 +6,21 @@ local M = {
   processors = require("structlog.processors"),
 }
 
+local loggers = {}
+
+function M.configure(logger_configs)
+  for name, config in pairs(logger_configs) do
+    loggers[name] = M.Logger(name, config.level, config.processors)
+  end
+end
+
+function M.get_logger(name)
+  local logger = loggers[name]
+  if not logger then
+    return nil
+  end
+
+  return vim.deepcopy(logger)
+end
+
 return M
---
+++ test/unit/init_spec.lua
@@ -0,0 +1,29 @@
+local log = require("structlog")
+
+describe("Module", function()
+  it("should expose interface details", function()
+    assert.equals("table", type(log.Logger))
+    assert.equals("table", type(log.level))
+    assert.equals("table", type(log.sinks))
+    assert.equals("table", type(log.processors))
+  end)
+end)
+
+describe("get_logger", function()
+  it("should return nil for unconfigured loggers", function()
+    local logger = log.get_logger("test")
+    assert.is.Nil(logger)
+  end)
+  it("should allow getting a configured logger", function()
+    local config = {
+      test = {
+        level = log.level.INFO,
+      },
+    }
+    log.configure(config)
+
+    local logger = log.get_logger("test")
+    assert.is_not.Nil(logger)
+    assert.equals(config.test.level, logger.level)
+  end)
+end)
