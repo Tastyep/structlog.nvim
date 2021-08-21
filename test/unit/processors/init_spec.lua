ecf1e4ebdc8bd53bc0d64c656dd9de23feee2d37
Luc Sinet <luc.sinet@gmail.com>
2021-08-21 11:29:52 +0200

    Add tests for Formatter

--- test/unit/processors/init_spec.lua
+++ test/unit/processors/init_spec.lua
@@ -10,3 +10,22 @@ describe("Timestamper", function()
     assert.equals(type(kwargs.timestamp), "string")
   end)
 end)
+
+describe("Formatter", function()
+  it("should format kwargs into a string", function()
+    local kwargs = { level = log.level.INFO, msg = "test", events = {} }
+    local formatter = processors.Formatter("[%s] %s", { "level", "msg" })
+
+    local message = formatter(vim.deepcopy(kwargs))
+    assert.equals(string.format("[%s] %s", kwargs.level, kwargs.msg), message)
+  end)
+
+  it("should format kwargs into a string and add remaining events if present", function()
+    local kwargs = { level = log.level.INFO, msg = "test", events = { test = "test" } }
+    local formatter = processors.Formatter("[%s] %s", { "level", "msg" })
+
+    local message = formatter(vim.deepcopy(kwargs))
+    local expected = string.format('[%s] %s test = "%s"', kwargs.level, kwargs.msg, kwargs.events.test)
+    assert.equals(expected, message)
+  end)
+end)
