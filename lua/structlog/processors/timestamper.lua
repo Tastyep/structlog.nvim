232a8e26060440e0db9fefba857036442b34227d
Luc Sinet <luc.sinet@gmail.com>
2022-05-27 10:08:12 +0200

    improvement(Timestamper): provide format documentation

--- lua/structlog/processors/timestamper.lua
+++ lua/structlog/processors/timestamper.lua
@@ -2,7 +2,13 @@
 
 --- Add a timestamp entry.
 -- @function Timestamper
--- @param format How to format the timestamp
+-- @param format The format string passed to os.date.
+--
+--  Format examples can be found in these two links:
+--
+--  https://devdocs.io/lua~5.4/index#pdf-os.date
+--
+--  https://www.lua.org/pil/22.1.html
 local function Timestamper(format)
   return function(_, kwargs)
     kwargs["timestamp"] = os.date(format)
