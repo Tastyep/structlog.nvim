b6affdbf7727e391de7a42d8cf1fcaf2383a4ca1
Luc Sinet <luc.sinet@gmail.com>
2023-01-02 10:17:01 +0100

    refactor: add basic pipeline test

---
+++ test/unit/pipeline_spec.lua
@@ -0,0 +1,23 @@
+local log = require("structlog")
+
+local stub = require("luassert.stub")
+local match = require("luassert.match")
+
+describe("pipeline", function()
+  local function make_test_log()
+    return { msg = "test", level = log.level.name(log.level.INFO) }
+  end
+
+  it("should route the log", function()
+    local formatter = function(log_entry)
+      return log_entry
+    end
+    local sink = {}
+    stub(sink, "write")
+
+    local pipeline = log.Pipeline:new(log.level.INFO, {}, formatter, sink)
+    local log_entry = make_test_log()
+    pipeline:push(log_entry)
+    assert.stub(sink.write).was_called_with(match.is_ref(sink), log_entry)
+  end)
+end)
