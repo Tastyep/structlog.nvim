5394de92d58b49fb53f82e69a76f876e98fdc9d5
Luc Sinet <luc.sinet@gmail.com>
2023-01-05 13:34:09 +0100

    feat: accept key=value args for building pipelines
    Mixing both string keys and numeric keys doesn't work as lua tables
    are hash maps, meaning that ordering is not guaranted.

--- test/unit/init_spec.lua
+++ test/unit/init_spec.lua
@@ -39,4 +39,28 @@ describe("get_logger", function()
     assert.are.equals(config.test.pipelines[1][3], logger.pipelines[1].formatter)
     assert.are.equals(config.test.pipelines[1][4], logger.pipelines[1].sink)
   end)
+
+  it("should accept string keys to construct pipeline objects", function()
+    local config = {
+      test = {
+        pipelines = {
+          {
+            level = log.level.INFO,
+            processors = {},
+            formatter = log.formatters.KeyValue,
+            sink = log.sinks.Console(),
+          },
+        },
+      },
+    }
+    log.configure(config)
+
+    local logger = log.get_logger("test")
+    assert.is_not.Nil(logger)
+    assert.are.equals(#config.test.pipelines, #logger.pipelines)
+    assert.are.equals(config.test.pipelines[1].level, logger.pipelines[1].level)
+    assert.are.equals(config.test.pipelines[1].processors, logger.pipelines[1].processors)
+    assert.are.equals(config.test.pipelines[1].formatter, logger.pipelines[1].formatter)
+    assert.are.equals(config.test.pipelines[1].sink, logger.pipelines[1].sink)
+  end)
 end)
